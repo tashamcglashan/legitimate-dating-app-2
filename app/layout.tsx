f8ef4930d57e0172af0dfbf337d00a5a6a44e64e
Tasha McGlashan <tmcglashan77@gmail.com>
2025-12-07 16:26:19 -0800

    trigger deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* ✅ Wrap everything in AppProvider */}
+        {/* ✅ Wrap everything in AppProvider please */}
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
